f56566ed44042ec43baac5da402e3771d417c4d6
Henry Till <henrytill@gmail.com>
2022-07-29 17:23:47 -0700

    add header, LICENSE

--- compile-commands.el
+++ compile-commands.el
@@ -1,3 +1,30 @@
+;;; compile-commands.el --- A library for working with JSON Compilation Database files  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Henry Till
+
+;; Author: Henry Till <henrytill@gmail.com>
+;; Keywords: c, convenience, languages
+
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (require 'json)
 (require 'project)
 
@@ -47,3 +74,4 @@
     (compile-commands--keys table)))
 
 (provide 'compile-commands)
+;;; compile-commands.el ends here
